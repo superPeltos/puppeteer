fcb98352e80cbf5228221abdc98ecfa8ce01acab
thibault <thibault@infra.fr>
2021-05-28 08:59:36 +0200

    remove .idea

--- index.js
+++ index.js
@@ -1,23 +1,28 @@
 const puppeteer = require('puppeteer');
-const yargs = require('yargs');
-const argv = yargs.argv;
-
-var http = require('http');
-var parseString = require('xml2js').parseString;
-var fs = require('fs');
-
-var options = {
+//const yargs = require('yargs');
+//const argv = yargs.argv;
+const http = require('http');
+const parseString = require('xml2js').parseString;
+const fs = require('fs');
+const {performance} = require('perf_hooks');
+const options = {
     host: 'preprod-client.eberhardt-pro.infra.fr',
     path: '/1_fr_0_sitemap.xml'
 }
-let packageData = [];
-let packageLength = 10;
 
+/**
+ * Package
+ */
+const packageLength = 10;
 let packageCount = 0;
 let packageKey = 0;
-let needle = '/c/';
-let fileName = 'crawler.json'
-const {performance} = require('perf_hooks');
+
+/**
+ * Package
+ */
+const needle = '/c/';
+const fileName = 'crawler.json'
+
 console.log('----------- INIT -------------');
 console.log('----------- INIT  TIMER-------------');
 let t0 = performance.now()
@@ -30,18 +35,20 @@ function deleteFile(){
     }
 }
 
-async function init(){
+function init(){
     if(fs.existsSync(fileName)) {
         deleteFile()
     }
-    var request = await http.request(options, function (res) {
-        var data = '';
+    let request = http.request(options, function (res) {
+        let data = '';
         res.on('data', function (chunk) {
             data += chunk;
         });
-        res.on('end',   function () {
-            createPackage(data);
+        res.on('end',   async function () {
+            let packageData = createPackage(data);
+            await crawlPackage(packageData)
         });
+
     });
     request.on('error', function (e) {
         console.log(e.message);
@@ -49,37 +56,8 @@ async function init(){
     request.end();
 }
 
-/**
- * Create pakage from request
- * @param data
- */
-async function createPackage(data){
+async function crawlPackage(packageData){
     let dataForJson = []
-    parseString(data, (err, res) => {
-        res.urlset.url.forEach((item) => {
-                if(packageCount < packageLength){
-                    if(packageData[packageKey] === undefined){
-                        packageData[packageKey] = []
-                        packageData[packageKey].push(item.loc[0])
-                        packageCount++
-                    }else{
-                        packageData[packageKey].push(item.loc[0])
-                        packageCount++
-                    }
-                }else{
-                    packageKey++;
-                    packageCount = 0;
-                    if(packageData[packageKey] === undefined){
-                        packageData[packageKey] = []
-                        packageData[packageKey].push(item.loc[0])
-                        packageCount++
-                    }else{
-                        packageData[packageKey].push(item.loc[0])
-                        packageCount++
-                    }
-                }
-        })
-    })
     for (let pack of packageData){
         //console.log('pack',pack)
         const promises = pack.map(crawlUrl);
@@ -98,9 +76,43 @@ async function createPackage(data){
         timerEnd()
         console.log('----------------------- Nothing found... -----------------------')
     }
-
-
-
+}
+/**
+ * Create pakage from request
+ * @param data
+ */
+function createPackage(data){
+    let packageData = []
+    parseString(data, (err, res) => {
+        res.urlset.url.forEach((item) => {
+            if(item.loc[0].search(needle) !== -1){
+                console.log(item.loc[0])
+                if(packageCount < packageLength){
+                        if(packageData[packageKey] === undefined){
+                            packageData[packageKey] = []
+                            packageData[packageKey].push(item.loc[0])
+                            packageCount++
+                        }else{
+                            packageData[packageKey].push(item.loc[0])
+                            packageCount++
+                        }
+                    }else{
+                        packageKey++;
+                        packageCount = 0;
+                        if(packageData[packageKey] === undefined){
+                            packageData[packageKey] = []
+                            packageData[packageKey].push(item.loc[0])
+                            packageCount++
+                        }else{
+                            packageData[packageKey].push(item.loc[0])
+                            packageCount++
+                        }
+                    }
+                }
+        })
+    })
+    console.log(packageData);
+    return packageData;
 
 }
 
@@ -119,6 +131,7 @@ function writeFile(data){
 async function crawlUrl(url){
     let data = null;
     let hrefs = [];
+    const needle = '/content/'
     console.log('------------------URL CRAWLED BEGIN-------------',url);
     const browserFetcher = puppeteer.createBrowserFetcher();
     const revisionInfo = await browserFetcher.download('869685');
@@ -127,15 +140,15 @@ async function crawlUrl(url){
     await page.setUserAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36');
     await page.setDefaultNavigationTimeout(0);
     await page.goto(url, {waitUntil: 'networkidle2'});
-    var hrefSearched = await page.$$("a");
+    //let hrefSearched = await page.$$(".elementor-video-wrapper"); ->  video
+    let hrefSearched = await page.$$("a");
 
     for( let link of hrefSearched ) {
+        //hrefs.push('here') ->> video
         const href = await page.evaluate(el => el.getAttribute("href"), link);
-        let needle = '/c/'
         if(href.search(needle) !== -1 && !hrefs.includes(href)){
             hrefs.push(href)
         }
-
     }
     if(hrefs.length === 0 ){
         console.log('------------------NO URL CRAWLER END-------------',url);
